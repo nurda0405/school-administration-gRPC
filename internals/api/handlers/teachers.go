faeb343d8afb1aad610147a5be1d9a7a74bdfadf
Nurdaulet <112893046+nurda0405@users.noreply.github.com>
2026-07-23 18:22:00 +0500

    Added filter for GetTeachers handler

--- internals/api/handlers/teachers.go
+++ internals/api/handlers/teachers.go
@@ -2,9 +2,14 @@ package handlers
 
 import (
 	"context"
+	"fmt"
+	"grpcapi/internals/models"
 	"grpcapi/internals/repositories/mongodb"
 	pb "grpcapi/proto/gen"
+	"reflect"
+	"strings"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -22,4 +27,43 @@ func (s *Server) AddTeachers(ctx context.Context, req *pb.Teachers) (*pb.Teacher
 	}
 
 	return &pb.Teachers{Teachers: addedTeachers}, nil
+}
+
+func (s *Server) GetTeachers(ctx context.Context, req *pb.GetTeachersRequest) (*pb.Teachers, error) {
+	buildFilterForTeacher(req)
+	return nil, nil
+}
+
+func buildFilterForTeacher(req *pb.GetTeachersRequest) {
+	filter := bson.M{}
+
+	var modelTeacher models.Teacher
+	modelVal := reflect.ValueOf(&modelTeacher).Elem()
+	modelType := modelVal.Type()
+
+	reqVal := reflect.ValueOf(req.Teacher).Elem()
+	reqType := reqVal.Type()
+
+	for i := 0; i < reqVal.NumField(); i++ {
+		fieldVal := reqVal.Field(i)
+		fieldName := reqType.Field(i).Name
+
+		if fieldVal.IsValid() && !fieldVal.IsZero() {
+			modelField := modelVal.FieldByName(fieldName)
+			if modelField.IsValid() && modelField.CanSet() {
+				modelField.Set(fieldVal)
+			}
+		}
+	}
+
+	for i := 0; i < modelVal.NumField(); i++ {
+		fieldVal := modelVal.Field(i)
+
+		if fieldVal.IsValid() && !fieldVal.IsZero() {
+			bsonTag := modelType.Field(i).Tag.Get("bson")
+			bsonTag = strings.TrimSuffix(bsonTag, ",omitempty")
+			filter[bsonTag] = fieldVal.Interface().(string)
+		}
+	}
+	fmt.Println(filter)
 }
